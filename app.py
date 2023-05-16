4d2261b909293ebfc0ccc7faeb9197a8179df23c
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-05-16 12:21:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st 
-import graphviz as graphviz
+import matplotlib.pyplot as plt
+import numpy as np
+
+rand=np.random.normal(1,2,size=20)
+fig, ax = plt.subplots()
+ax.hist(rand,bins=15)
+st.pyplot(fig)
+
+"""
+st.title("app title")
+st.sidebar.title("this is side bar")
+
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
 
 st.graphviz_chart('''
   digraph {
@@ -7,3 +20,4 @@ st.graphviz_chart('''
     Tuna -> dong-won
     }
 ''')
+"""
