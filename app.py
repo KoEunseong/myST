cba55a2f27fc148e15fa06bb902967bcfd1f06e5
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-05-16 12:17:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st 
 import graphviz as graphviz
-st.title("app title")
-st.sidebar.title("this is side bar")
-
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
 
 st.graphviz_chart('''
   digraph {
