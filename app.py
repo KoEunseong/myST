96b7aa4ce4fe773e7940579438c1649eff8ca3e5
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-05-16 11:32:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st 
+import graphviz as graphviz
 st.title("app title")
 st.sidebar.title("this is side bar")
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
+
