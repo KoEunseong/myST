19c2fe01870ae9158f2790b0f87dd205fd0897dc
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-06-17 02:35:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import streamlit as st 
+import streamlit as st
+import osmnx as ox
+import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
-rand=np.random.normal(1,2,size=20)
-fig, ax = plt.subplots()
-ax.hist(rand,bins=15)
+# 페이지 제목 설정
+st.title("OSMnx 도로 네트워크 시각화")
+
+# 사용자로부터 도시 이름 입력 받기
+city_name = "New York, USA"
+
+graph = ox.graph_from_place(city_name, network_type="all")
+    # st.success("도로 네트워크 데이터 로드 완료#!")
+    # st.error("도로 네트워크 데이터를 로드하는 중 오류가 발생했습니다.")
+
+# 도로 네트워크 시각화
+# if graph is not None:
+#     # 도로 네트워크 그래프를 Matplotlib Figure로 변환
+#     fig, ax = ox.plot_graph(graph, show=False, close=False)
+
+    # Matplotlib Figure를 Streamlit에 표시
 st.pyplot(fig)
-
-"""
-st.title("app title")
-st.sidebar.title("this is side bar")
-
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
-
-st.graphviz_chart('''
-  digraph {
-    Big_shark -> Tuna
-    Tuna -> dong-won
-    }
-''')
-"""
